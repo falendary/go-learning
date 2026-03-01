c9d4bb4d2d6ae5d20a6d25e800fdc596db43a86b
Sergei Zhitenev <s.zhitenev@finmars.com>
2026-03-01 11:22:26 +0100

    update examples for slices

--- slice/main.go
+++ slice/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"slices"
 	"sort"
+	"strings"
 )
 
 func main() {
@@ -46,6 +47,54 @@ func main() {
 
 	sort.Strings(names)
 	fmt.Println("sorted names", names)
+
+	// Filters
+	evens := filter(nums, func(n int) bool { return n%2 == 0 })
+	fmt.Println("even nums:", evens)
+
+	// Join slices
+
+	first := []int{100, 200}
+	second := []int{300, 400}
+
+	//Why is it needed?
+	//
+	//The append function looks like this:
+	//
+	//append(slice []T, elems ...T) []T
+	//
+	//Notice ...T.
+	//
+	//	That means:
+	//
+	//The second parameter is variadic
+	//
+	//It expects individual values, not a slice
+
+	first = append(first, second...) // The ... is called the variadic expansion operator.
+	fmt.Println("Joined slices", first)
+
+	//Using slices.Concat
+	third := []int{500, 600}
+	forth := []int{700, 800}
+
+	joined := slices.Concat(third, forth)
+	fmt.Println("Joined using slices.Concat", joined)
+
+	// Find item
+	// if initialization; condition {
+	if idx, ok := findIndex(names, "Kate"); ok {
+		fmt.Println("Name Kate is found", idx)
+	} else {
+		fmt.Println("Kate is not found")
+	}
+
+	// Replace item
+	names = replaceFirst(names, "Serge", "Sergei")
+	fmt.Println("Replace name", names)
+
+	// Join Strings
+	fmt.Println("name strings", strings.Join(names, ", "))
 }
 
 // removeAt removes element at index i, preserving order
@@ -80,5 +129,29 @@ func findIndex[T comparable](s []T, target T) (int, bool) {
 	}
 
 	return -1, false
+
+}
+
+// filter returns a new slice containing items for which keep(item) == true
+func filter[T any](s []T, keep func(T) bool) []T {
+
+	out := make([]T, 0, len(s))
+
+	for _, val := range s {
+		if keep(val) {
+			out = append(out, val)
+		}
+	}
+
+	return out
+}
+
+func replaceFirst[T comparable](s []T, old T, new T) []T {
+
+	if idx, ok := findIndex(s, old); ok {
+		s[idx] = new
+	}
+
+	return s
 
 }
